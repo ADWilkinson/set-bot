863c60999aae5ddb6c25fabd4bb8d684b508b8a3
Andy <andrew.wilkinson@r3.com>
2021-07-10 19:46:46 +0100

    init

---
+++ addressMap.js
@@ -0,0 +1,8 @@
+exports.addressMap = {
+  '0x68ab9f9ba8E490D9Dc191f7A90540b5EDCcC92D6': 'Crypto Kai Innovation Index',
+  '0xD13bF4acF5d4b2407d785d2528D746fA75CF9778': 'Crypto Kai Blue Chip Index',
+  '0x1494CA1F11D487c2bBe4543E90080AeBa4BA3C2b': 'DeFi Pulse Index',
+  '0x72e364F2ABdC788b7E918bc238B21f109Cd634D7': 'Metaverse Index',
+  '0xAa6E8127831c9DE45ae56bB1b0d4D4Da6e5665BD': 'ETH 2X Flexible Leverage Index',
+  '0x0B498ff89709d3838a063f1dFA463091F9801c2b': 'BTC 2X Flexible Leverage Index',
+};
---
+++ index.js
@@ -0,0 +1,153 @@
+const scraperClient = require('scraperapi-sdk')('d668ceea885d0125abbd1a112775c6a1');
+const fs = require('fs');
+const map = require('./addressMap');
+const { Webhook } = require('discord-webhook-node');
+const hook = new Webhook(
+  'https://discord.com/api/webhooks/863477263928262656/FBbtS8RxgU6GT-ICODLN_F3ePVeUfvzB7AaNyRJtOG5J9wYEX0oc2KyC1Q9hSnYLqC3d'
+);
+var cron = require('node-cron');
+
+const runScrape = async () => {
+  Object.keys(map.addressMap).forEach(async (key, index) => {
+    setTimeout(async function () {
+      const page = await scraperClient.get('https://www.tokensets.com/v2/set/' + key, {
+        render: true,
+      });
+      console.log(page);
+
+      fs.writeFile('./scrapingData/' + key + '.txt', page, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+    }, index * 1000);
+  });
+};
+
+const diff = (a, b) => {
+  let numA = parseFloat(a.replace('$', ''));
+  let numB = parseFloat(b.replace('$', ''));
+  return ((100 * (numB - numA)) / ((numB + numA) / 2)).toFixed(2);
+};
+
+const runReports = async () => {
+  var files = fs.readdirSync('./scrapingData').map((file) => file.replace('.txt', ''));
+
+  files.forEach((set) => {
+    fs.readFile('./scrapingData/' + set + '.txt', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const length = 15;
+      const aumIndex = data.indexOf('>$');
+      const aum = data.substr(aumIndex, length);
+      const convertedAum = '$' + aum.replace(',', '').match(/[\d\.\,]+/)[0];
+      console.log(convertedAum);
+
+      const slice = data.slice(aumIndex + length, data.length);
+      const tokenPriceIndex = slice.indexOf('>$');
+      const tokenPrice = slice.substr(tokenPriceIndex, length);
+      const convertedTokenPrice = '$' + tokenPrice.replace(',', '').match(/[\d\.\,]+/)[0];
+      console.log(convertedTokenPrice);
+
+      try {
+        const report = JSON.parse(fs.readFileSync('./report/' + set + '.json', 'utf8'));
+
+        console.log(report);
+        let result = {
+          name: map.addressMap[set],
+          aum: convertedAum,
+          price: convertedTokenPrice,
+          aumChange: diff(convertedAum, report.aum),
+          priceChange: diff(convertedTokenPrice, report.price) + '%',
+        };
+        console.log(result);
+        fs.writeFile('./report/' + set + '.json', JSON.stringify(result), (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        });
+      } catch (error) {
+        let result = {
+          name: map.addressMap[set],
+          aum: convertedAum,
+          price: convertedTokenPrice,
+          aumChange: '0%',
+          priceChange: '0%',
+        };
+        console.log(result);
+        fs.writeFile('./report/' + set + '.json', JSON.stringify(result), (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        });
+      }
+    });
+  });
+};
+
+const runGetMessage = async () => {
+  let message = '';
+  var files = fs.readdirSync('./report');
+  const reportArr = files.map((file) => {
+    return JSON.parse(fs.readFileSync('./report/' + file, 'utf8'));
+  });
+
+  const sortedArr = reportArr.sort((a, b) => parseFloat(b.priceChange) - parseFloat(a.priceChange));
+
+  message += '**Set Performance Leaderboard** 📈\n\n';
+  sortedArr.forEach((set, index) => {
+    if (index == 0) {
+      message += `🥇 **${set.name}**\nPrice: ${set.price} -- Change (%): ${set.priceChange} | AUM: ${set.aum} -- Change (%): ${set.aumChange}\n`;
+    } else if (index == 1) {
+      message += `🥈 **${set.name}**\nPrice: ${set.price} -- Change (%): ${set.priceChange} | AUM: ${set.aum} -- Change (%): ${set.aumChange}\n`;
+    } else if (index == 2) {
+      message += `🥉 **${set.name}**\nPrice: ${set.price} -- Change (%): ${set.priceChange} | AUM: ${set.aum} -- Change (%): ${set.aumChange}\n`;
+    } else {
+      message += `**${set.name}**\nPrice: ${set.price} -- Change (%): ${set.priceChange} | AUM: ${set.aum} -- Change (%): ${set.aumChange}\n`;
+    }
+  });
+  return message;
+};
+
+const runPostMessageToDiscord = async (message) => {
+  try {
+    await hook.send(message);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const runProcess = async () => {
+  await runScrape();
+  await runReports();
+
+  setTimeout(async () => {
+    const message = await runGetMessage();
+    await runPostMessageToDiscord(message);
+  }, 5000);
+};
+
+runProcess()
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+
+cron.schedule('0 */4 * * *', () => {
+  console.log('running a task every minute');
+  await runScrape();
+});
+
+cron.schedule('5 */4 * * *', () => {
+  console.log('running a task every minute');
+  await runReports();
+});
+
+cron.schedule('10 */4 * * *', () => {
+  const message = await runGetMessage();
+  await runPostMessageToDiscord(message);
+});
