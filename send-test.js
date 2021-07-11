be5bdb720474748479f9cead3191ff4386c33e1e
Andy <andrew.wilkinson@r3.com>
2021-07-11 08:28:37 +0100

    Update send-test.js

--- send-test.js
+++ send-test.js
@@ -1,4 +1,4 @@
-const { runPostMessageToDiscord } = require('./index');
+const { runGetMessage, runPostMessageToDiscord } = require('./index');
 const { Webhook } = require('discord-webhook-node');
 const hook = new Webhook(
   'https://discord.com/api/webhooks/863667320077680690/nURYUFO_2Gaq-TZyxU8MHDDhkTrl9XaviihDsolQvAH6CNJ_Y6G7QXnyj9nyBC4bdYtr'
