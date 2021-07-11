ac49d6cadaa823995d5e17d04e5f99ae9336fd69
Andy <andrew.wilkinson@r3.com>
2021-07-11 08:39:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.config = {
-  apiKey: 'd668ceea885d0125abbd1a112775c6a1',
+  apiKey: '',
   prodWebhook:
-    'https://discord.com/api/webhooks/863477263928262656/FBbtS8RxgU6GT-ICODLN_F3ePVeUfvzB7AaNyRJtOG5J9wYEX0oc2KyC1Q9hSnYLqC3d',
+    '',
   testWebhook:
-    'https://discord.com/api/webhooks/863667320077680690/nURYUFO_2Gaq-TZyxU8MHDDhkTrl9XaviihDsolQvAH6CNJ_Y6G7QXnyj9nyBC4bdYtr',
+    '',
 };
